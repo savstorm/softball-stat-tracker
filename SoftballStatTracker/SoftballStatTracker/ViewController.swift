9a05679b8ed192618340838d6b213a32cac8db7f
SavannaHunt <savannastormhunt@gmail.com>
2018-12-24 13:34:46 -0500

    initial application structure

---
+++ SoftballStatTracker/SoftballStatTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftballStatTracker
+//
+//  Created by Savanna Hunt on 12/24/18.
+//  Copyright © 2018 Savanna Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
